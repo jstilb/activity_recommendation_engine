488dad755259362c4e276bcacd2ba885af69c259
JM Snow <jstilb7@gmail.com>
2020-08-25 17:18:24 -0600

    connect gspread incomplete

--- activity_rec_engine.py
+++ activity_rec_engine.py
@@ -1,1 +1,13 @@
-print("git please connect")
+import pandas as pd
+import gspread
+
+gc = gspread.oauth()
+
+sh = gc.open("Activity Recommendation Engine")
+
+print(sh.sheet1.get('A1'))
+
+#test data frame
+d = {'col1': [1, 2], 'col2': [3, 4]}
+df = pd.DataFrame(data=d)
+print(df)
