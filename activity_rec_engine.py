b89caf7025f7c4c47149d84d43cc852738bfbbeb
JM Snow <jstilb7@gmail.com>
2020-08-27 17:36:19 -0700

    set up log sheet and dataframe

--- activity_rec_engine.py
+++ activity_rec_engine.py
@@ -2,12 +2,30 @@ import pandas as pd
 import gspread
 from info import *
 
+#google sheets connection & DataFrame creation
 gc = gspread.service_account(filename=fname)
-
 sh = gc.open(spsh)
-ws = sh.worksheet("Activities")
-data = ws.get_all_values()
-headers = data.pop(0)
 
-df = pd.DataFrame(data, columns=headers)
-print(df)
+act_ws = sh.worksheet("Activities")
+act_data = act_ws.get_all_values()
+act_headers = act_data.pop(0)
+act_df = pd.DataFrame(act_data, columns=act_headers)
+
+log_ws = sh.worksheet("Activity Log")
+log_data = log_ws.get_all_values()
+log_headers = log_data.pop(0)
+log_df = pd.DataFrame(log_data, columns=log_headers)
+
+#need to include only activities that are able to be completed or planned during current season
+
+#check Activity Log for recent item
+
+
+#create recommendation & update activity log
+
+
+
+#print(df)
+
+
+#def small_activity_recommendation():
