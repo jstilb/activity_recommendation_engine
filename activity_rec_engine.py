fba8c02cebdd5714bdb5f252e9b5042cf046ea49
JM Snow <jstilb7@gmail.com>
2020-09-16 17:43:10 -0600

    cleaned & refactored slightly

--- activity_rec_engine.py
+++ activity_rec_engine.py
@@ -4,10 +4,10 @@ from info import *
 import random
 from twilio.rest import Client
 
-#Twilio Information
+# Twilio Information
 client = Client(account_sid, auth_token)
 
-#google sheets connection & DataFrame creation
+# google sheets connection & DataFrame creation
 gc = gspread.service_account(filename=fname)
 sh = gc.open(spsh)
 
@@ -17,15 +17,16 @@ act_df = pd.DataFrame(act_ws.get_all_records())
 log_ws = sh.worksheet("Recommendation Log")
 log_df = pd.DataFrame(log_ws.get_all_records())
 
-#need to include only activities that are able to be completed or planned during current season
 
-#check Activity Log for recent item to create parameter for recommendaiton engine
+# need to include only activities that are able to be completed or planned during current season
+
+# check Activity Log for recent item to create parameter for recommendaiton engine
 def check_log(log):
     recent_activities = log.tail(10)
     count_easy_act = 0
     count_med_act = 0
     count_hard_act = 0
-    plan_dif_rec = 0 # 1 = easy; 2 = med; 3 = hard
+    plan_dif_rec = 0  # 1 = easy; 2 = med; 3 = hard
     for val in recent_activities['Planning Difficulty']:
         if val == "easy":
             count_easy_act += 1
@@ -42,10 +43,9 @@ def check_log(log):
     return plan_dif_rec
 
 
-
-#create recommendation using check_log return, priority, and rand. Update actvities and activity log
+# create recommendation using check_log return, priority, and rand. Update actvities and activity log
 def recommendation_engine(cl_return, act):
-    #set parameters for recommendation using priority & planning difficulty
+    # set parameters for recommendation using priority & planning difficulty
     dif_options = ["easy", "med", "hard"]
     priority_options = ["low", "med", "high"]
     temp_df = pd.DataFrame(act[act["Planning Difficulty"].str.match(dif_options[cl_return - 1])])
@@ -55,7 +55,7 @@ def recommendation_engine(cl_return, act):
         df = temp_df[temp_df["Priority"].str.match(priority_options[-counter])].reset_index(drop=True)
         counter += 1
 
-    #make recommendation
+    # make recommendation
     recommendation = df.loc[random.randrange(len(df.index))]
     message = client.messages \
         .create(
@@ -63,22 +63,15 @@ def recommendation_engine(cl_return, act):
         from_=twilio_phone,
         to=jm_phone
     )
-    #message.sid
+    message.sid
 
-    #update recommendation log
+    # update recommendation log
     new_log_df = log_df.append(recommendation).reset_index(drop=True).fillna('')
     log_ws.update([new_log_df.columns.values.tolist()] + new_log_df.values.tolist())
 
-    #delete row in activities worksheet
+    # delete row in activities worksheet
     row_to_delete = act_ws.find(recommendation.get(0)).row
     act_ws.delete_rows(row_to_delete)
 
 
-
-
-
-
-
-
 recommendation_engine(check_log(log_df), act_df)
-
